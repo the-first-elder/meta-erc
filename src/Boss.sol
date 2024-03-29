064ccfbfb2a8edb00a3111e9fb5730f708828e90
forge-innovate <emmanuelmayowa95@gmail.com>
2024-03-29 14:01:03 +0100

    edited

--- src/Boss.sol
+++ src/Boss.sol
@@ -4,13 +4,16 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-address owner;
 
 contract Boss is ERC20 {
+address public owner;
+
     constructor()
         ERC20("Boss", "B")
-        owner = msg.sender;
-    {}
+       
+    {
+         owner = msg.sender;
+    }
 
 modifier onlyOwner(){
     require(msg.sender != owner, "not owner");
