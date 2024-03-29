3a6bd7f90bc4e817e266c863aa2b5544f5b2e533
forge-innovate <emmanuelmayowa95@gmail.com>
2024-03-29 13:37:22 +0100

    changed contract

--- src/Boss.sol
+++ src/Boss.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -17,4 +17,9 @@ contract Boss is ERC20 {
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
+
+    function transfer(address newUser, uint amount) public override  returns (bool valid) {
+        valid = super.transfer(newUser, amount);
+    }
 }
+
