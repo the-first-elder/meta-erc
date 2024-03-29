62b0ed90dabc4bdcdba0d07d4d46913498698c69
forge-innovate <emmanuelmayowa95@gmail.com>
2024-03-29 13:59:40 +0100

    added modifier

--- src/Boss.sol
+++ src/Boss.sol
@@ -4,22 +4,26 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+address owner;
 
 contract Boss is ERC20 {
     constructor()
         ERC20("Boss", "B")
-        
+        owner = msg.sender;
     {}
 
-    function mint(address to, uint256 amount) public  {
+modifier onlyOwner(){
+    require(msg.sender != owner, "not owner");
+    _;
+}
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
+    function burn(address user, uint256 amount) public onlyOwner {
+        _burn(user, amount);
     }
 
     function transfer(address newUser, uint amount) public override  returns (bool valid) {
         valid = super.transfer(newUser, amount);
     }
 }
-
